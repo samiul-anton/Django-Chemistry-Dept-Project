e3442a81811ab908d2a02289e14398e6fa9b81ca
tahsin_dev <tahsinahmed52225@gmail.com>
2022-08-07 18:08:44 +0600

    Feat: Lab resouce edit added

--- chemistryDept/resourceApp/url.py
+++ chemistryDept/resourceApp/url.py
@@ -6,9 +6,13 @@ from django.conf.urls.static import static
 
 
 urlpatterns =[
+  #lab resource url
   path('all-lab-facility/', views.labFacility, name='admin_lab_facilites'),
   path('add/all-lab-facility/', views.addLabFacility, name='add_lab_facilites'),
   path('all-lab-facility/<int:id>/delete', views.deleteLabFacility, name='delete_lab_facilites'),
+  path('all-lab-facility/edit/<int:id>', views.editLabFacility, name='edit_lab_facilites'),
+   path('all-lab-facility/get/<int:id>', views.getLabResource, name='get_lab_facilites'),
+  #computing url
   path('all-computing/', views.computing, name='admin_computing'),
   path('add-computing/', views.addComputing, name='add_computing'),
   path('all-computing/<int:id>/delete', views.deleteComputing, name='delete_computing'),
