72c86d297db10afdb50a2351b89772b26093a527
tahsin_dev <tahsinahmed52225@gmail.com>
2022-08-14 11:08:19 +0600

    faculty delete option fixed

--- chemistryDept/static/js/deleteData.js
+++ chemistryDept/static/js/deleteData.js
@@ -1,6 +1,7 @@
 $(window).on('load', function() {
 
     $('.del-btn').on("click",function(){
+
         $("#delete-form").attr("action",`/super-admin/`+$(this).attr('data-page')+`/`+$(this).attr('data-id')+`/delete`);
     });
 
